6f6fede653690c42390a1f02bebc4d94c6c8c0f9
Nasrinpatel <89184631+Nasrinpatel@users.noreply.github.com>
2026-02-27 13:27:19 +0530

    slider

--- platform/themes/ninico/partials/shortcodes/simple-slider/styles/fashion.blade.php
+++ platform/themes/ninico/partials/shortcodes/simple-slider/styles/fashion.blade.php
@@ -2,41 +2,50 @@
     <div class="swiper-containers slidertwo-active">
         <div class="swiper-wrapper">
             @foreach($sliders as $slider)
-                <div class="swiper-slide slider-bg" @if ($shortcode->background_color) style="background-color: {{ $shortcode->background_color }} !important;" @endif>
-                    <div class="container">
-                        <div class="slider-top-padding pt-55">
-                            <div class="row p-relative align-items-end">
-                                <div class="col-xl-5 col-lg-6 col-md-6 d-flex align-self-center">
-                                    <div class="tpslidertwo__item">
-                                        <div class="tpslidertwo__content">
-                                            @if($subtitle = $slider->getMetaData('subtitle', true))
-                                                <h4 class="tpslidertwo__sub-title">{!! BaseHelper::clean($subtitle) !!}</h4>
-                                            @endif
-                                            @if($slider->title)
-                                                    <h3 class="tpslidertwo__title mb-10">{!! BaseHelper::clean($slider->title) !!}</h3>
-                                            @endif
-                                            @if($description = $slider->description)
-                                                <div>{!! BaseHelper::clean($description) !!}</div>
-                                            @endif
-                                            @if(($actionLabel = $slider->getMetaData('action_label', true)) && $slider->link)
-                                                <div class="tpslidertwo__slide-btn mt-20">
-                                                    <a class="tp-btn banner-animation" href="{{ $slider->link }}">
-                                                        {{ $actionLabel }}
-                                                        <i class="fal fa-long-arrow-right"></i>
-                                                    </a>
-                                                </div>
-                                            @endif
-                                        </div>
-                                    </div>
-                                </div>
-                                <div @class(['col-xl-7 col-lg-6 col-md-6', 'd-none d-md-block' => ! $shortcode->show_slider_image_on_mobile])>
-                                    <div class="tpslidertwo__img p-relative text-end">
-                                        @include(Theme::getThemeNamespace('partials.shortcodes.simple-slider.includes.image', compact('slider')))
+                <div class="swiper-slide slider-bg"
+                    style="background-image: url('{{ RvMedia::getImageUrl($slider->image) }}');
+                            background-size: cover;
+                            background-position: center;">
+
+                    <div class="slider-content-wrapper">
+                        <div class="container">
+                            <div class="slider-top-padding pt-120 pb-120">
+                                <div class="tpslidertwo__item">
+                                    <div class="tpslidertwo__content text-white">
+
+                                        @if($subtitle = $slider->getMetaData('subtitle', true))
+                                            <h4 class="tpslidertwo__sub-title text-white">
+                                                {!! BaseHelper::clean($subtitle) !!}
+                                            </h4>
+                                        @endif
+
+                                        @if($slider->title)
+                                            <h3 class="tpslidertwo__title mb-10 text-white">
+                                                {!! BaseHelper::clean($slider->title) !!}
+                                            </h3>
+                                        @endif
+
+                                        @if($description = $slider->description)
+                                            <div class="text-white">
+                                                {!! BaseHelper::clean($description) !!}
+                                            </div>
+                                        @endif
+
+                                        @if(($actionLabel = $slider->getMetaData('action_label', true)) && $slider->link)
+                                            <div class="tpslidertwo__slide-btn mt-20">
+                                                <a class="tp-btn banner-animation" href="{{ $slider->link }}">
+                                                    {{ $actionLabel }}
+                                                    <i class="fal fa-long-arrow-right"></i>
+                                                </a>
+                                            </div>
+                                        @endif
+
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+
                 </div>
             @endforeach
         </div>
@@ -49,3 +58,4 @@
         </div>
     </div>
 </div>
+
