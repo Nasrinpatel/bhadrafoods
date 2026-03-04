c5b2b9817333a4bf48b51a10e81e788cb65688f3
fidaalivohra <30294306+fidaalivohra@users.noreply.github.com>
2026-03-04 23:50:45 +0530

    Update footer.blade.php

--- platform/themes/ninico/partials/footer.blade.php
+++ platform/themes/ninico/partials/footer.blade.php
@@ -7,10 +7,16 @@
         --footer-text-color: {{ theme_option('footer_text_color', '#000000') }};
         --footer-text-muted-color: {{ theme_option('footer_text_muted_color', '#686666') }};
         --footer-border-color: {{ theme_option('footer_border_color', '#E0E0E0') }};
+        --footer-bottom-background-color: {{ theme_option('footer_bottom_background_color', '#ededed') }};
+        --footer-bottom-text-color: {{ theme_option('footer_bottom_text_color', '#ffffff') }};
     }
     .footer-area{
         background-color: var(--footer-background-color);
         color: var(--footer-text-color);
+    }
+    .footer-copyright {
+        background-color: var(--footer-bottom-background-color);
+        color: var(--footer-bottom-text-color);
     }
 </style>
 <footer>
@@ -29,7 +35,7 @@
                 </div>
             </div>
         </div>
-        <div class="footer-copyright" style="background-color: {{ Theme::get('footerBottomBackgroundColor') ?: theme_option('footer_bottom_background_color', '#ededed') }}">
+        <div class="footer-copyright">
             <div class="container">
                 <div class="row">
                     {!! dynamic_sidebar('footer_bottom_sidebar') !!}
